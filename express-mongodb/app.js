3b26962bbc11f82c51180be8c4d1c0d4936ce1a2
larkersos <larker@foxmail.com>
2018-07-25 09:15:11 +0800

    express-mongodb

---
+++ express-mongodb/app.js
@@ -0,0 +1,60 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+//var session = require('express-session');
+//var MongoStore = require('connect-mongo')(session);
+//var settings = require('./config/config-db');
+//app.use(session({
+//  secret: settings.cookieSecret,
+//  key: settings.db,//cookie name
+//  cookie: {maxAge: 1000 * 60 * 60 * 24 * 30},//30 days
+//  store: new MongoStore({
+//    db: settings.db,
+//    host: settings.host,
+//    port: settings.port
+//  })
+//}));
+//
+//var flash = require('connect-flash');
+//app.use(flash());
+
+
+module.exports = app;
---
+++ express-mongodb/config/config-db.js
@@ -0,0 +1,9 @@
+/**
+ * 数据库配置
+ */
+module.exports = {
+    cookieSecret: 'express-demo',
+    db: 'git-express-demo',
+    host: 'localhost',
+    port: 27017
+};
